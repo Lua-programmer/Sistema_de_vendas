dd699d17ca4df52c4dc99a684ab92b3522e7dbc2
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-10-25 20:30:10 -0300

    Configurando o flyway

---
+++ src/main/resources/db/migrations/V01__criar_e_registrar_categorias.sql
@@ -0,0 +1,10 @@
+CREATE TABLE categoria (
+	codigo BIGINT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
+
+INSERT INTO categoria (nome) values ('Tecnologia');
+INSERT INTO categoria (nome) values ('Acessórios para veículos');
+INSERT INTO categoria (nome) values ('Esporte e Lazer');
+INSERT INTO categoria (nome) values ('Casa e Eletrodomésticos');
+INSERT INTO categoria (nome) values ('Joias e Relógios');
