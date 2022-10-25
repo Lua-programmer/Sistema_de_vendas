2d1078f8528ab09795b0476cbf20ecab241148ec
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-10-25 14:48:06 -0300

    Subindo a aplicação

---
+++ src/main/java/com/vendas/gestavendas/GestaVendasApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.gestavendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendas/gestavendas/GestaVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendas.gestavendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
