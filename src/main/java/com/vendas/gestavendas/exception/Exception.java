83d85ace79b0f91e05a77632c7298b91afd16b1f
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-10-31 13:27:55 -0300

    Tratando exceção ao atualizar uma categoria inexistente

---
+++ src/main/java/com/vendas/gestavendas/exception/Exception.java
@@ -0,0 +1,51 @@
+package com.vendas.gestavendas.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class Exception extends ResponseEntityExceptionHandler {
+    private static final String CONSTANT_VALIDATION_NOT_BLANK = "NotBlank";
+    private static final String CONSTANT_VALIDATION_LENGTH = "Length";
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        List<Errors> errors = errorsList(ex.getBindingResult());
+
+        return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Errors> errorsList(BindingResult bindingResult) {
+        List<Errors> errorsList = new ArrayList<>();
+
+        bindingResult.getFieldErrors().forEach(fieldErrors -> {
+            String msgUser = handleUserErrorMessage(fieldErrors);
+            String msgServer = fieldErrors.toString();
+            errorsList.add(new Errors(msgUser, msgServer));
+        });
+
+        return errorsList;
+    }
+
+    private String handleUserErrorMessage(FieldError fieldErrors) {
+        if (fieldErrors.getCode().equals(CONSTANT_VALIDATION_NOT_BLANK)) {
+            return fieldErrors.getDefaultMessage().concat(" é obrigatório.");
+        }
+        if (fieldErrors.getCode().equals(CONSTANT_VALIDATION_LENGTH)) {
+            return fieldErrors.getDefaultMessage().concat(String.format(" deve ter entre %s e %s caracteres", fieldErrors.getArguments()[2], fieldErrors.getArguments()[1]));
+        }
+        return fieldErrors.toString();
+    }
+}
