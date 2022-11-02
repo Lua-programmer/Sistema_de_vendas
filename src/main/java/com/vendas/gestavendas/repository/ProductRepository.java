e089d01669acef8f3a72d826a796b1ef8bcaf42a
Luana Melissa <luanamelissaprogrammer@gmail.com>
2022-11-01 22:23:17 -0300

    Validando produto já cadastrado e lançando exceção com Exception Handler

--- src/main/java/com/vendas/gestavendas/repository/ProductRepository.java
+++ src/main/java/com/vendas/gestavendas/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package com.vendas.gestavendas.repository;
 import com.vendas.gestavendas.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ProductRepository extends JpaRepository<Product, UUID> {
+    Optional<Product> findByCategoryCodeAndDescription(UUID codeCategory, String description);
 }
--- src/main/java/com/vendas/gestavendas/service/impl/ProductImpl.java
+++ src/main/java/com/vendas/gestavendas/service/impl/ProductImpl.java
@@ -1,6 +1,7 @@
 package com.vendas.gestavendas.service.impl;
 
 import com.vendas.gestavendas.entity.Product;
+import com.vendas.gestavendas.exception.RuleBusinessException;
 import com.vendas.gestavendas.repository.ProductRepository;
 import com.vendas.gestavendas.service.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import java.util.UUID;
 public class ProductImpl implements ProductService {
 
     private final ProductRepository productRepository;
+    private final CategoryImpl categoryService;
 
     @Override
     public List<Product> getProducts() {
@@ -28,6 +30,23 @@ public class ProductImpl implements ProductService {
 
     @Override
     public Product saveProduct(Product product) {
+        validateExistingProductCategory(product.getCategory().getCode());
+        validateDuplicateCategoryCode(product);
         return productRepository.save(product);
+    }
+
+    private void validateDuplicateCategoryCode(Product product) {
+        if (productRepository.findByCategoryCodeAndDescription(product.getCategory().getCode(), product.getDescription()).isPresent()) {
+            throw new RuleBusinessException(String.format("The product [ %s ] is already registered", product.getDescription()));
+        }
+    }
+
+    private void validateExistingProductCategory(UUID code) {
+        if (code == null) {
+            throw new RuleBusinessException("Category code must not be null");
+        }
+        if (categoryService.getCategoryByCode(code).isEmpty()) {
+            throw new RuleBusinessException(String.format("Category code %s not exists", code));
+        }
     }
 }
